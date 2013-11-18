9787071384e97a285a17c5f9480e8e6a61a9a4b2
Ben Lewis <bennlewis@gmail.com>
2013-11-18 13:24:14 -0700

    built REST API using express, mongoose, path, and node

--- site/js/views/book.js
+++ site/js/views/book.js
@@ -5,6 +5,18 @@ app.BookView = Backbone.View.extend({
   className: 'bookContainer',
   template: _.template( $('#bookTemplate').html() ),
 
+  events: {
+    'click .delete': 'deleteBook'
+  },
+
+  deleteBook: function() {
+    // Delete model
+    this.model.destroy();
+
+    // Delete view
+    this.remove();
+  },
+
   render: function() {
     // tmpl is a function that takes a JSON object and returns html
 
